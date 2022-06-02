0cdf535758e65e9c1fa434c157aa395e95ec0fbd
MubasharAli2020 <22mubasharali@gmail.com>
2022-06-02 09:19:11 +0500

    change

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -2,48 +2,26 @@ import {React} from 'react';
 import './Contact.css';
 export default function Contact(){
     return(
-        <div className='contact container'>
-            <div className='row'>
-                <div className='col-md-6'>
-                    <img src="https://images.all-free-download.com/images/graphicwebp/communication_icons_modern_men_sketch_cartoon_characters_6844995.webp" />
-                </div>
-               <div className='col-md-4 offset-1'>
-               <div class="container d-flex justify-content-center align-items-center">
-		<form className='form form-items'>
-		<h1 class="title text-center mb-4">Talk to Us</h1>
-
-			
-			<div class="form-group position-relative">
-				<label for="formName" class="d-block">
-					<i class="icon" data-feather="user"></i>
-				</label>
-				<input type="text" id="formName" class="form-control form-control-lg thick" placeholder="Name" />
-			</div>
-
-			
-			<div class="form-group position-relative">
-				<label for="formEmail" class="d-block">
-					<i class="icon" data-feather="mail"></i>
-				</label>
-				<input type="email" id="formEmail" class="form-control form-control-lg thick" placeholder="E-mail" />
-			</div>
-
-			
-			<div class="form-group message">
-				<textarea id="formMessage" class="form-control" rows="7" placeholder="Message"></textarea>
-			</div>
+		<div className='contact'>	
 		
-			
-			<div class="text-center button-submit">
-				<button type="submit" class="submit-btn btn btn-primary" tabIndex="-1">Send message</button>
-			</div>
-	</form>
-	
-</div>
-               </div>
-            
-            </div>
 
-        </div>
+		<div class="container">
+		<div class="right"></div>
+		<div class="left">
+		  <div class="header">
+			<h2 class="animation a1">Contact US</h2>
+			<h4 class="animation a2">Contact US for any further inqueries</h4>
+		  </div>
+		  <div class="form">
+			<input type="email" class="form-field animation a3" placeholder="Email Address" />
+			<input type="password" class="form-field animation a4" placeholder="Password" />
+			<textarea class="form-field animation a4" placeholder="Message" rows="7" ></textarea>
+		   
+			<button class="animation a6">Send</button>
+		  </div>
+		</div>
+		
+	  </div>
+	  </div>
     )
 }
